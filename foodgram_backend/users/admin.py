85ef9348072d4dca1986381b358f89be7e0ef34c
aten88 <virtuoso_ten@mail.ru>
2023-07-05 19:01:41 +0300

    Описал приложение users и зарегал через адм

--- foodgram_backend/users/admin.py
+++ foodgram_backend/users/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@register(User)
+class MyUserAdmin(UserAdmin):
+    list_display = (
+        'pk', 'username', 'email', 'first_name', 'last_name', 'password',
+    )
+    list_filter = ('username', 'email', )
+    search_fields = ('username', 'email', )
