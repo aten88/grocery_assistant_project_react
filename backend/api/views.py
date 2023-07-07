a5b57e4b3a906ccf2f894200a878a39d1e6d08a7
aten88 <virtuoso_ten@mail.ru>
2023-07-07 19:03:24 +0300

    Описал Тагвьюсет

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import (
+    AllowAny
+)
 
-# Create your views here.
+from recipes.models import (
+    Tag
+)
+from .serializers import TagSerializer
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для модели Tag."""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (AllowAny,)
