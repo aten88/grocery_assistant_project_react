f5e5409fee7b5a258b38d7bda5c7af2cc1815b4e
Алексей Тен <virtuoso_ten@mail.ru>
2023-07-10 23:20:40 +0300

    Доб Djoser юзеры доб-ся но работает как то криво

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'recipes',
     'api',
     
@@ -127,3 +129,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER = {
+    'USER_ID_FIELD': 'email',
+    'USER_CREATE_PASSWORD_RETYPE': False,
+}
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(('api.urls', 'api')))
+    path('api/', include(('api.urls', 'api'))),
+    path('api/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
