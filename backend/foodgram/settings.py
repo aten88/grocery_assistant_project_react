018a11b14081edfcf08ad4a8eaa34d3eee5d4aaa
aten88 <virtuoso_ten@mail.ru>
2023-07-18 19:14:13 +0300

    Из-нил кол-во эл-тов в пагинаторе в глоб настр

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    'PAGE_SIZE': 5,
 }
 DJOSER = {
     'LOGIN_FIELD': 'email',
