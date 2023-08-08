bc281892240bda6576eac3dd9fadcd3d0412c0f0
Алексей Тен <virtuoso_ten@mail.ru>
2023-08-08 21:18:37 +0300

    Добавил UniqueConstraint к моделям

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 from django.contrib.auth.models import User
 from django.core.validators import (
     RegexValidator, MinValueValidator, MaxValueValidator
@@ -64,21 +65,13 @@ class Ingredient(models.Model):
     )
 
     class Meta:
-        unique_together = ('name', 'measurement_unit')
+        constraints = [
+            UniqueConstraint(
+                fields=['name', 'measurement_unit'], name='unique_ingredient'
+            )
+        ]
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
-
-    def clean(self):
-        '''Метод валидации данных перед сохранением.'''
-        existing_ingredient = Ingredient.objects.filter(
-            name=self.name,
-            measurement_unit=self.measurement_unit
-        ).first()
-
-        if existing_ingredient and existing_ingredient.pk != self.pk:
-            raise ValidationError(
-                'Такой ингредиент с такой единицей измерения уже существует.'
-            )
 
     def __str__(self) -> str:
         return self.name
@@ -164,7 +157,12 @@ class RecipeIngredient(models.Model):
     )
 
     class Meta:
-        unique_together = ('recipe', 'ingredient')
+        constraints = [
+            UniqueConstraint(
+                fields=['recipe', 'ingredient'],
+                name='unique_recipe_ingredient'
+            )
+        ]
         verbose_name = 'Ингредиент в рецепте'
         verbose_name_plural = 'Ингредиенты в рецептах'
 
@@ -189,6 +187,11 @@ class Favorite(models.Model):
     )
 
     class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'recipe'], name='unique_favorite'
+            )
+        ]
         verbose_name = 'Избранное'
         verbose_name_plural = 'Избранное'
 
@@ -198,15 +201,6 @@ class Favorite(models.Model):
             raise ValidationError(
                 'Вы не можете добавить свой рецепт в избранное.'
             )
-
-    def validate_unique(self, exclude=None):
-        '''Метод для проверки уникальности данных перед сохранением.'''
-        super().validate_unique(exclude)
-
-        if Favorite.objects.filter(
-            user=self.user, recipe=self.recipe
-        ).exists():
-            raise ValidationError('Этот рецепт уже добавлен в избранное.')
 
     def __str__(self) -> str:
         '''Метод строкового представления модели.'''
@@ -230,7 +224,11 @@ class Subscription(models.Model):
     )
 
     class Meta:
-        unique_together = ('author', 'user')
+        constraints = [
+            UniqueConstraint(
+                fields=['author', 'user'], name='unique_subscription'
+            )
+        ]
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
 
@@ -262,7 +260,11 @@ class ShoppingCart(models.Model):
     )
 
     class Meta:
-        unique_together = ('user', 'recipe')
+        constraints = [
+            UniqueConstraint(
+                fields=['user', 'recipe'], name='unique_shopping_cart'
+            )
+        ]
         verbose_name = 'Список покупок'
         verbose_name_plural = 'Списки покупок'
 
