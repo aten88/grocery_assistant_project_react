6dba486d4d89b347956aa4527c6193ac3745cb81
Алексей Тен <virtuoso_ten@mail.ru>
2023-08-23 01:06:25 +0300

    Сделал валидацию для admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 from .models import (
     Tag, Ingredient, Recipe, Favorite,
@@ -6,14 +7,56 @@ from .models import (
 )
 
 
+class TagAdminForm(forms.ModelForm):
+    '''Форма валидации модели Tag.'''
+    class Meta:
+        model = Tag
+        fields = ['name', 'color', ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        color = cleaned_data.get('color')
+
+        if Tag.objects.filter(name=name).exclude(id=self.instance.id).exists():
+            raise forms.ValidationError('Тег с таким именем уже существует.')
+
+        if Tag.objects.filter(color=color).exclude(
+            id=self.instance.id
+        ).exists():
+            raise forms.ValidationError('Тег с таким цветом уже существует.')
+
+        return cleaned_data
+
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    form = TagAdminForm
+
+
+class IngredientAdminForm(forms.ModelForm):
+    '''Форма валидации модели Ingredient.'''
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'measurement_unit', ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+
+        if Ingredient.objects.filter(name=name).exclude(
+            id=self.instance.id
+        ).exists():
+            raise forms.ValidationError(
+                'Ингредиент с таким названием уже существует.'
+            )
+
+        return cleaned_data
 
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    pass
+    form = IngredientAdminForm
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -30,19 +73,93 @@ class RecipeAdmin(admin.ModelAdmin):
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
-    pass
+    '''Форма валидации модели Favorite.'''
+    def save_model(self, request, obj, form, change):
+        user = request.user
+        recipe = obj.recipe
+
+        if user == recipe.author:
+            raise forms.ValidationError(
+                'Вы не можете добавить свой рецепт в избранное.'
+            )
+
+        if recipe.favorites.filter(user=user).exists():
+            raise forms.ValidationError(
+                'Данный рецепт уже добавлен в избранное.'
+            )
+        obj.user = user
+        obj.save()
+
+
+class SubscriptionAdminForm(forms.ModelForm):
+    '''Форма валидации модели Subscription.'''
+    class Meta:
+        model = Subscription
+        fields = ['author', 'user', ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        author = cleaned_data.get('author')
+        user = cleaned_data.get('user')
+
+        if author == user:
+            raise forms.ValidationError('Ошибка валидации.')
+
+        if author.follow.filter(user=user).exclude(
+            id=self.instance.id
+        ).exists():
+            raise forms.ValidationError('Ошибка валидации.')
+
+        return cleaned_data
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    pass
+    form = SubscriptionAdminForm
+
+
+class ShoppingCartAdminForm(forms.ModelForm):
+    '''Форма валидации модели ShoppingCart.'''
+    class Meta:
+        model = ShoppingCart
+        fields = ['user', 'recipe', ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user = cleaned_data.get('user')
+        recipe = cleaned_data.get('recipe')
+
+        if user.shopping_user.filter(recipe=recipe).exclude(
+            id=self.instance.id
+        ).exists():
+            raise forms.ValidationError('Ошибка валидации.')
+
+        return cleaned_data
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
-    pass
+    form = ShoppingCartAdminForm
+
+
+class RecipeIngredientAdminForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        recipe = cleaned_data.get('recipe')
+        ingredient = cleaned_data.get('ingredient')
+
+        if recipe.recipe_ingredients_set.filter(ingredient=ingredient).exclude(
+            id=self.instance.id
+        ).exists():
+            raise forms.ValidationError('Ошибка валидации.')
+
+        return cleaned_data
 
 
 @admin.register(RecipeIngredient)
 class RecipeIngredientAdmin(admin.ModelAdmin):
-    pass
+    form = RecipeIngredientAdminForm
