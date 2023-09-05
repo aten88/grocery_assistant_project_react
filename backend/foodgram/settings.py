f55acc7c048e577b19206b66465b820063b4b441
aten88 <virtuoso_ten@mail.ru>
2023-09-05 10:18:07 +0300

    Настроил volume на sqlite для foodgram

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
 
